ee09f91becee1a8c64f8b55e90f68799e8555a0c
BiMalxMe <journeyxbimal@gmail.com>
2025-09-07 06:29:54 +0545

    Chores : Some Minor Fixings

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
+// main internal logic
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <WalletContextProvider>
